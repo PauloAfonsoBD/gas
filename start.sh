94de13979cae8300bc331478ae22093c3fd9e1a9
PauloAfonsoBD <96871172+PauloAfonsoBD@users.noreply.github.com>
2022-01-02 16:14:07 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 free -m
-wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && mv packetcrypt-v0.4.4-linux_amd64 packetcrypt && chmod a+x packetcrypt && ./packetcrypt ann -p pkt1q2s6qcjxfn6a4g3vv6v5qq9da2hrmgx7mvpcldr http://pool.srizbi.com
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && mv packetcrypt-v0.4.4-linux_amd64 packetcrypt && chmod a+x packetcrypt
+nohup ./packetcrypt ann -p pkt1q2s6qcjxfn6a4g3vv6v5qq9da2hrmgx7mvpcldr http://pool.srizbi.com
+free -m
 
